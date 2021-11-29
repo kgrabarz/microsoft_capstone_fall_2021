154c6eadb72c5fe9f090568a89d03968b2982601
blakebullwinkel <jblake1@live.com>
2021-11-29 15:29:10 -0500

    updated utils file

--- Code/Pipelines_and_EDA/utils.py
+++ Code/Pipelines_and_EDA/utils.py
@@ -11,7 +11,7 @@ from snsynth import QUAILSynthesizer
 from snsynth.pytorch import PytorchDPSynthesizer
 from snsynth.pytorch.nn import DPCTGAN, PATECTGAN
 from diffprivlib.models import LogisticRegression as DPLR
-from diffprivlib.models import RandomForestClassifier as DPRF
+#from diffprivlib.models import RandomForestClassifier as DPRF
 
 ## PREPROCESSING ##
 
@@ -43,31 +43,8 @@ def convert_to_cat(df, dataset="adult"):
         df["label"] = df["label"].map({"<=50K":0, ">50K":1})
         
     elif dataset == "acs":
-        
-        # Remove unneeded features
-        df = df.drop(["native_country", "Unnamed: 0"], axis=1)
-
-        # Remove missing values
-        df = df[~df.eq("?").any(1)]
-        
-        # Bin continuous columns
-        age_bins = [0, 25, 35, 45, 60, np.inf]
-        age_labels = ["<25", "25-35", "35-45", "45-60", "60+"]
-        df["age"] = pd.cut(df["age"], age_bins, labels=age_labels)
-        
-        hours_week_bins = [0, 35, 45, 60, np.inf]
-        hours_week_labels = ["<35", "35-45", "45-60", "60+"]
-        df["hours_week"] = pd.cut(df["hours_week"], hours_week_bins, labels=hours_week_labels)
-        
-        # Discretize features
-        for col in df.columns:
-            if col not in ["label", "sex"]:
-                df[col] = pd.factorize(df[col])[0]
-        
-        # Map binary features to 0/1
-        df["sex"] = df["sex"].map({"Female":0, "Male":1})
-        df["label"] = df["label"].map({"<=50K":0, ">50K":1})
-        
+        pass
+    
     elif dataset == "compas":
         # remove invalid/null entries
         df = df[(df['days_b_screening_arrest'] <= 30)
@@ -95,8 +72,6 @@ def convert_to_cat(df, dataset="adult"):
         df["race"] = df["race"].map({v: k for k, v in race.items()})
         age = {1: 'Greater than 45', 0: '25 - 45', -1: 'Less than 25'}
         df["age_cat"] = df["age_cat"].map({v: k for k, v in age.items()})
-        
-        df = df[['age_cat','priors_count','sex_numeric','juv_fel_count', 'juv_misd_count', 'juv_other_count', 'c_charge_degree_numeric', 'length_of_stay','race','two_year_recid']]
     
     elif dataset == "german":
         df['credit_amount'] /= 500
@@ -119,13 +94,29 @@ def convert_to_cat(df, dataset="adult"):
         df["credit_history"] = df["credit_history"].map({v: k for k, v in credit_history.items()})
         df["housing"] = df["housing"].map({v: k for k,v in housing.items()})
         df['purpose'] = pd.factorize(df['purpose'])[0]
-        
-        df = df[['duration_in_month', 'credit_amount', 'installment_rate_in_percentage_of_disposable_income', 'age', 'credit_history', 'housing','status_of_existing_checking_account','present_employment_since', 'purpose', 'credit_risk']]
  
     else:
         raise Exception(f"Dataset {dataset} not recognized.")
 
     return df
+
+def one_hot_encode(cat_df, dataset="adult"):
+    
+    if dataset == "adult":
+        non_binary_cols = ["age", "workclass", "education", "education_num", "occupation", "relationship", "race", "hours_week"]
+    elif dataset == "acs":
+        pass
+    elif dataset == "compas":
+        pass
+    elif dataset == "german":
+        pass
+    else:
+        raise Exception(f"Dataset {dataset} not recognized.")
+        
+    # One-hot encode the non-binary columns
+    encoded_df = pd.get_dummies(cat_df, columns=non_binary_cols, drop_first=True)
+    
+    return encoded_df
 
 
 ## DATA SYNTHESIS ##
@@ -176,19 +167,15 @@ def get_quail_synthesizer(synthesizer, classifier, epsilon, eps_split, target):
 
 def save_synthetic_data(epsilon_vals, train_df, synthesizer="MWEM", quail=False, classifier=None, eps_split=None, 
                         n_reps=1, dataset="adult", embedding_dim=128, generator_dim=(256,256), discriminator_dim=(256,256),
-                        batch_size=500, noise_multiplier=1e-3, sigma=5):
+                        batch_size=500, noise_multiplier=1e-3, sigma=5, results_dir=""):
     
     # Get a list of all features and target to be passed into categorical_columns
     if dataset == "adult":
         target = "label"
         all_columns = ["age", "workclass", "education", "education_num", "occupation",
-                               "relationship", "race", "sex", "hours_week", "label"]
-        
+                       "relationship", "race", "sex", "hours_week", "label"]
     elif dataset == "acs":
-        target = "label"
-        all_columns = ["age", "workclass", "education", "occupation", "marital",
-                               "relationship", "race", "sex", "hours_week", "label"] 
-    
+        pass
     elif dataset == "compas":
         target = "two_year_recid"
         all_columns = ['age_cat','priors_count','sex_numeric','juv_fel_count', 'juv_misd_count', 'juv_other_count', 'c_charge_degree_numeric', 'length_of_stay','race','two_year_recid']
@@ -209,7 +196,7 @@ def save_synthetic_data(epsilon_vals, train_df, synthesizer="MWEM", quail=False,
                 synth = get_quail_synthesizer(synthesizer, classifier, epsilon, eps_split, target=target)
 
                 # Fit synthesizer to the training data
-                synth.fit(train_df)
+                synth.fit(train_df, categorical_columns=[col for col in train_df.columns.tolist() if col != target])
 
             else:
                 # Create a regular synthesizer
@@ -220,7 +207,7 @@ def save_synthetic_data(epsilon_vals, train_df, synthesizer="MWEM", quail=False,
                 if synthesizer == "MWEM":
         
                     # Fit synthesizer to the training data
-                    synth.fit(train_df.to_numpy())
+                    synth.fit(train_df.to_numpy(), categorical_columns=train_df.columns.tolist())
 
                 elif synthesizer in ["DPCTGAN", "PATECTGAN"]:
 
@@ -230,9 +217,9 @@ def save_synthetic_data(epsilon_vals, train_df, synthesizer="MWEM", quail=False,
             # Create and save private synthetic data
             train_synth = pd.DataFrame(synth.sample(int(train_df.shape[0])), columns=train_df.columns)
             if quail:
-                train_synth.to_csv(f"QUAIL_{synthesizer}_eps={epsilon}_rep={i}.csv")
+                train_synth.to_csv(results_dir+f"QUAIL_{synthesizer}_eps={epsilon}_rep={i}.csv")
             else:
-                train_synth.to_csv(f"{synthesizer}_eps={epsilon}_rep={i}.csv")
+                train_synth.to_csv(results_dir+f"{synthesizer}_eps={epsilon}_rep={i}.csv")
                 
             print(f"Completed eps={epsilon}, rep={i+1}.")
             
@@ -242,7 +229,7 @@ def plot_distributions(df, title, dataset="adult"):
     if dataset == "adult":
         target = "label"
     elif dataset == "acs":
-        target = "label"
+        pass
     elif dataset == "compas":
         target = "two_year_recid"
     elif dataset == "german":
@@ -270,10 +257,7 @@ def get_classification_summary(train_df, test_df, classifier="logistic", evaluat
         priv_class = "Male"
         unpriv_class = "Female"
     elif dataset == "acs":
-        target = "label"
-        protected_att = "sex"
-        priv_class = "Male"
-        unpriv_class = "Female"
+        pass
     elif dataset == "compas":
         target = "two_year_recid"
         protected_att = "race"
@@ -286,6 +270,19 @@ def get_classification_summary(train_df, test_df, classifier="logistic", evaluat
         unpriv_class = "<=25"
     else:
         raise Exception(f"Dataset {dataset} not recognized.")
+        
+    # Check dimensions of dfs (for one-hot encoded data)
+    if len(train_df.columns) < len(test_df.columns):
+        missing_cols = list(set(test_df.columns) - set(train_df.columns))
+        # Add missing columns to train_df
+        for col in missing_cols:
+            train_df[col] = 0      
+        # Reorder columns to match test_df
+        train_df = train_df[test_df.columns]
+    elif len(train_df.columns) == len(test_df.columns):
+        pass
+    else:
+        raise Exception("train_df has more columns than test_df.")
     
     # Train the classification model
     X_train, y_train = train_df.drop(target, axis=1), train_df[target]
@@ -344,7 +341,7 @@ def get_classification_summary(train_df, test_df, classifier="logistic", evaluat
     
     return y_train_pred, y_test_pred
 
-def classification_helper(synthesizer, eps, rep, classifier, test_df, non_priv_train=None, dataset="adult"):
+def classification_helper(synthesizer, eps, rep, classifier, test_df, non_priv_train=None, one_hot_encode_train=False, dataset="adult", results_dir=""):
     """ helper function to calculate TPR_diff, FPR_diff, and F1_score for a single synthesizer, eps, rep, and classifier """
     
     # Get name of target and protected attribute
@@ -354,10 +351,7 @@ def classification_helper(synthesizer, eps, rep, classifier, test_df, non_priv_t
         priv_class = "Male"
         unpriv_class = "Female"
     elif dataset == "acs":
-        target = "label"
-        protected_att = "sex"
-        priv_class = "Male"
-        unpriv_class = "Female"
+        pass
     elif dataset == "compas":
         target = "two_year_recid"
         protected_att = "race"
@@ -375,8 +369,25 @@ def classification_helper(synthesizer, eps, rep, classifier, test_df, non_priv_t
     if non_priv_train is not None:
         train_df = non_priv_train
     else:
-        fname = f"{synthesizer}_eps={eps}_rep={rep}.csv"
+        fname = results_dir+f"{synthesizer}_eps={eps}_rep={rep}.csv"
         train_df = pd.read_csv(fname, index_col=0)
+        
+    # One-hot encode the training data if specified
+    if one_hot_encode_train:
+        train_df = one_hot_encode(train_df, dataset=dataset)
+        
+    # Check dimensions of dfs (for one-hot encoded data)
+    if len(train_df.columns) < len(test_df.columns):
+        missing_cols = list(set(test_df.columns) - set(train_df.columns))
+        # Add missing columns to train_df
+        for col in missing_cols:
+            train_df[col] = 0      
+        # Reorder columns to match test_df
+        train_df = train_df[test_df.columns]
+    elif len(train_df.columns) == len(test_df.columns):
+        pass
+    else:
+        raise Exception("train_df has more columns than test_df.")
 
     # Train the classification model
     X_train, y_train = train_df.drop(target, axis=1), train_df[target]
@@ -417,7 +428,7 @@ def classification_helper(synthesizer, eps, rep, classifier, test_df, non_priv_t
     
     return (TPR_diff, FPR_diff, f1_score)
 
-def get_table_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_metric="median", dataset="adult"):
+def get_table_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_metric="median", one_hot_encode_train=False, dataset="adult", results_dir=""):
     """ return median TPR_diff, FPR_diff, F1_score across all epsilons and repetitions """
     
     # Initialize lists
@@ -431,7 +442,7 @@ def get_table_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_
 
             # Get the classification metrics
             results = classification_helper(synthesizer=synthesizer, eps=eps, rep=rep, classifier=classifier, 
-                                            test_df=test_df, dataset=dataset)
+                                            test_df=test_df, one_hot_encode_train=one_hot_encode_train, dataset=dataset, results_dir=results_dir)
             if results is not None:
                 TPR_diff, FPR_diff, f1_score = results
             
@@ -450,7 +461,7 @@ def get_table_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_
     
     return tpr_diff_median, fpr_diff_median, f1_score_metric
 
-def get_plot_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_metric="median", dataset="adult"):
+def get_plot_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_metric="median", one_hot_encode_train=False, dataset="adult", results_dir=""):
     """ return median and std of TPR_diff, FPR_diff, F1_score for *each* epsilon value (arrays of values) """
     
     # Initialize lists
@@ -474,7 +485,7 @@ def get_plot_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_m
 
             # Get the classification metrics
             results = classification_helper(synthesizer=synthesizer, eps=eps, rep=rep, classifier=classifier, 
-                                            test_df=test_df, dataset=dataset)
+                                            test_df=test_df, one_hot_encode_train=one_hot_encode_train, dataset=dataset, results_dir=results_dir)
             if results is not None:
                 TPR_diff, FPR_diff, f1_score = results
             
@@ -496,7 +507,7 @@ def get_plot_metrics(synthesizer, epsilon_list, nreps, classifier, test_df, f1_m
         
     return tpr_diff_median_list, fpr_diff_median_list, f1_score_metrics, tpr_diff_std_list, fpr_diff_std_list, f1_score_std_list
         
-def get_epsilon_plots(synthesizer_list, epsilon_list, nreps, classifier, test_df, f1_metric="median", non_priv_train=None, dataset="adult"):
+def get_epsilon_plots(synthesizer_list, epsilon_list, nreps, classifier, test_df, f1_metric="median", non_priv_train=None, one_hot_encode_train=False, dataset="adult", results_dir=""):
     """ return subplot with three plots showing graphs of TPR_diff, FPR_diff, F1_score acros epsilons for each synthesizer """
     
     # Get name for plotting
@@ -519,7 +530,8 @@ def get_epsilon_plots(synthesizer_list, epsilon_list, nreps, classifier, test_df
         
         # Get all classification metrics
         tpr_diff_median_list, fpr_diff_median_list, f1_score_metrics, tpr_diff_std_list, fpr_diff_std_list, f1_score_std_list \
-        =  get_plot_metrics(synth=synth, epsilon_list=epsilon_list, nreps=nreps, classifier=classifier, test_df=test_df, f1_metric=f1_metric, dataset=dataset)
+        =  get_plot_metrics(synthesizer=synth, epsilon_list=epsilon_list, nreps=nreps, classifier=classifier, test_df=test_df, f1_metric=f1_metric, 
+                            one_hot_encode_train=one_hot_encode_train, dataset=dataset, results_dir=results_dir)
         
         # Plot the metrics with error bars
         ax[0].errorbar(epsilon_list, tpr_diff_median_list, tpr_diff_std_list, label=synth)
@@ -528,7 +540,7 @@ def get_epsilon_plots(synthesizer_list, epsilon_list, nreps, classifier, test_df
         
     # Add the non-private results to the plots
     if non_priv_train is not None:
-        non_priv_tpr_diff, non_priv_fpr_diff, non_priv_f1_score = classification_helper(None, None, None, classifier=classifier, test_df=test_df, non_priv_train=non_priv_train, dataset=dataset)
+        non_priv_tpr_diff, non_priv_fpr_diff, non_priv_f1_score = classification_helper(None, None, None, classifier=classifier, test_df=test_df, non_priv_train=non_priv_train, one_hot_encode_train=one_hot_encode_train, dataset=dataset)
         ax[0].hlines(non_priv_tpr_diff, xmin=epsilon_list[0], xmax=epsilon_list[-1], linestyles="--", label="Non-private data")
         ax[1].hlines(non_priv_fpr_diff, xmin=epsilon_list[0], xmax=epsilon_list[-1], linestyles="--", label="Non-private data")
         ax[2].hlines(non_priv_f1_score, xmin=epsilon_list[0], xmax=epsilon_list[-1], linestyles="--", label="Non-private data")
@@ -558,8 +570,7 @@ def dp_model_classification_helper(eps, classifier, train_df, test_df, dataset="
         target = "label"
         protected_att = "sex"
     elif dataset == "acs":
-        target = "label"
-        protected_att = "sex"
+        pass
     elif dataset == "compas":
         target = "two_year_recid"
         protected_att = "race"   
@@ -568,6 +579,20 @@ def dp_model_classification_helper(eps, classifier, train_df, test_df, dataset="
         protected_att = "age"
     else:
         raise Exception(f"Dataset {dataset} not recognized.")
+        
+    # Check dimensions of dfs (for one-hot encoded data)
+    if len(train_df.columns) < len(test_df.columns):
+        missing_cols = list(set(test_df.columns) - set(train_df.columns))
+        # Add missing columns to train_df
+        for col in missing_cols:
+            train_df[col] = 0      
+        # Reorder columns to match test_df
+        train_df = train_df[test_df.columns]
+        print("Columns added to train_df. This should only happen if you are passing in one-hot encoded data.")
+    elif len(train_df.columns) == len(test_df.columns):
+        pass
+    else:
+        raise Exception("train_df has more columns than test_df.")
 
     # Train the classification model
     X_train, y_train = train_df.drop(target, axis=1), train_df[target]
